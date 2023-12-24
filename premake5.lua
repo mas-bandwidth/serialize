2ce6fd3e9c9a0674aedccc5570cc0a3f312bf5a3
Glenn Fiedler <glenn@networknext.com>
2023-12-24 17:20:24 -0500

    split apart useful serialize stuff into its own library

---
+++ premake5.lua
@@ -0,0 +1,81 @@
+
+solution "Serialize"
+    kind "ConsoleApp"
+    language "C++"
+    configurations { "Debug", "Release" }
+    includedirs { "." }
+    if not os.istarget "windows" then
+        targetdir "bin/"  
+    end
+    rtti "Off"
+    warnings "Extra"
+    floatingpoint "Fast"
+    filter "configurations:Debug"
+        symbols "On"
+        defines { "SERIALIZE_DEBUG" }
+    filter "configurations:Release"
+        symbols "Off"
+        optimize "Speed"
+        defines { "SERIALIZE_RELEASE" }
+
+project "test"
+    files { "test.cpp", "serialize.h" }
+    defines { "SERIALIZE_ENABLE_TESTS=1" }
+
+newaction
+{
+    trigger     = "clean",
+
+    description = "Clean all build files and output",
+
+    execute = function ()
+
+        files_to_delete = 
+        {
+            "Makefile",
+            "*.make",
+            "*.txt",
+            "*.zip",
+            "*.tar.gz",
+            "*.db",
+            "*.opendb",
+            "*.vcproj",
+            "*.vcxproj",
+            "*.vcxproj.user",
+            "*.vcxproj.filters",
+            "*.sln",
+            "*.xcodeproj",
+            "*.xcworkspace"
+        }
+
+        directories_to_delete = 
+        {
+            "obj",
+            "ipch",
+            "bin",
+            ".vs",
+            "Debug",
+            "Release",
+            "release",
+            "cov-int",
+            "docs",
+            "xml"
+        }
+
+        for i,v in ipairs( directories_to_delete ) do
+          os.rmdir( v )
+        end
+
+        if not os.istarget "windows" then
+            os.execute "find . -name .DS_Store -delete"
+            for i,v in ipairs( files_to_delete ) do
+              os.execute( "rm -f " .. v )
+            end
+        else
+            for i,v in ipairs( files_to_delete ) do
+              os.execute( "del /F /Q  " .. v )
+            end
+        end
+
+    end
+}
---
+++ test.cpp
@@ -0,0 +1,324 @@
+/*
+    serialize
+
+    Copyright © 2016 - 2024, Mas Bandwidth LLC.
+
+    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+        1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+        2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
+           in the documentation and/or other materials provided with the distribution.
+
+        3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived
+           from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+    WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+    USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "serialize.h"
+#include <time.h>
+
+using namespace serialize;
+
+static void CheckHandler( const char * condition,
+                          const char * function,
+                          const char * file,
+                          int line )
+{
+    printf( "check failed: ( %s ), function %s, file %s, line %d\n", condition, function, file, line );
+#ifndef NDEBUG
+    #if defined( __GNUC__ )
+        __builtin_trap();
+    #elif defined( _MSC_VER )
+        __debugbreak();
+    #endif
+#endif
+    exit( 1 );
+}
+
+#define check( condition )                                                     \
+do                                                                             \
+{                                                                              \
+    if ( !(condition) )                                                        \
+    {                                                                          \
+        CheckHandler( #condition, __FUNCTION__, __FILE__, __LINE__ );          \
+    }                                                                          \
+} while(0)
+
+void test_endian()
+{
+    uint32_t value = 0x11223344;
+
+    const char * bytes = (const char*) &value;
+
+#if SERIALIZE_LITTLE_ENDIAN
+
+    check( bytes[0] == 0x44 );
+    check( bytes[1] == 0x33 );
+    check( bytes[2] == 0x22 );
+    check( bytes[3] == 0x11 );
+
+#else // #if SERIALIZE_LITTLE_ENDIAN
+
+    check( bytes[3] == 0x44 );
+    check( bytes[2] == 0x33 );
+    check( bytes[1] == 0x22 );
+    check( bytes[0] == 0x11 );
+
+#endif // #if SERIALIZE_LITTLE_ENDIAN
+}
+
+void test_bitpacker()
+{
+    const int BufferSize = 256;
+
+    uint8_t buffer[BufferSize];
+
+    BitWriter writer( buffer, BufferSize );
+
+    check( writer.GetData() == buffer );
+    check( writer.GetBitsWritten() == 0 );
+    check( writer.GetBytesWritten() == 0 );
+    check( writer.GetBitsAvailable() == BufferSize * 8 );
+
+    writer.WriteBits( 0, 1 );
+    writer.WriteBits( 1, 1 );
+    writer.WriteBits( 10, 8 );
+    writer.WriteBits( 255, 8 );
+    writer.WriteBits( 1000, 10 );
+    writer.WriteBits( 50000, 16 );
+    writer.WriteBits( 9999999, 32 );
+    writer.FlushBits();
+
+    const int bitsWritten = 1 + 1 + 8 + 8 + 10 + 16 + 32;
+
+    check( writer.GetBytesWritten() == 10 );
+    check( writer.GetBitsWritten() == bitsWritten );
+    check( writer.GetBitsAvailable() == BufferSize * 8 - bitsWritten );
+
+    const int bytesWritten = writer.GetBytesWritten();
+
+    check( bytesWritten == 10 );
+
+    memset( buffer + bytesWritten, 0, BufferSize - bytesWritten );
+
+    BitReader reader( buffer, bytesWritten );
+
+    check( reader.GetBitsRead() == 0 );
+    check( reader.GetBitsRemaining() == bytesWritten * 8 );
+
+    uint32_t a = reader.ReadBits( 1 );
+    uint32_t b = reader.ReadBits( 1 );
+    uint32_t c = reader.ReadBits( 8 );
+    uint32_t d = reader.ReadBits( 8 );
+    uint32_t e = reader.ReadBits( 10 );
+    uint32_t f = reader.ReadBits( 16 );
+    uint32_t g = reader.ReadBits( 32 );
+
+    check( a == 0 );
+    check( b == 1 );
+    check( c == 10 );
+    check( d == 255 );
+    check( e == 1000 );
+    check( f == 50000 );
+    check( g == 9999999 );
+
+    check( reader.GetBitsRead() == bitsWritten );
+    check( reader.GetBitsRemaining() == bytesWritten * 8 - bitsWritten );
+}
+
+void test_bits_required()
+{
+    check( bits_required( 0, 0 ) == 0 );
+    check( bits_required( 0, 1 ) == 1 );
+    check( bits_required( 0, 2 ) == 2 );
+    check( bits_required( 0, 3 ) == 2 );
+    check( bits_required( 0, 4 ) == 3 );
+    check( bits_required( 0, 5 ) == 3 );
+    check( bits_required( 0, 6 ) == 3 );
+    check( bits_required( 0, 7 ) == 3 );
+    check( bits_required( 0, 8 ) == 4 );
+    check( bits_required( 0, 255 ) == 8 );
+    check( bits_required( 0, 65535 ) == 16 );
+    check( bits_required( 0, 4294967295 ) == 32 );
+}
+
+const int MaxItems = 11;
+
+struct TestData
+{
+    TestData()
+    {
+        memset( this, 0, sizeof( TestData ) );
+    }
+
+    int a,b,c;
+    uint32_t d : 8;
+    uint32_t e : 8;
+    uint32_t f : 8;
+    bool g;
+    uint32_t v32;
+    uint64_t v64;
+    int numItems;
+    int items[MaxItems];
+    float float_value;
+    float compressed_float_value;
+    double double_value;
+    uint64_t uint64_value;
+    uint8_t bytes[17];
+    char string[256];
+};
+
+struct TestContext
+{
+    int min;
+    int max;
+};
+
+struct TestObject
+{
+    TestData data;
+
+    void Init()
+    {
+        data.a = 1;
+        data.b = -2;
+        data.c = 150;
+        data.d = 55;
+        data.e = 255;
+        data.f = 127;
+        data.g = true;
+        data.v32 = UINT32_MAX;
+        data.v64 = UINT64_MAX;
+
+        data.numItems = MaxItems / 2;
+        for ( int i = 0; i < data.numItems; ++i )
+            data.items[i] = i + 10;
+
+        // todo: bring back
+        //data.compressed_float_value = 2.13f;
+        //data.float_value = 3.1415926f;
+
+        data.double_value = 1 / 3.0;
+        data.uint64_value = 0x1234567898765432L;
+
+        for ( int i = 0; i < (int) sizeof( data.bytes ); ++i )
+            data.bytes[i] = rand() % 255;
+
+        strcpy( data.string, "hello world!" );
+    }
+
+    template <typename Stream> bool Serialize( Stream & stream )
+    {
+        const TestContext & context = *(const TestContext*) stream.GetContext();
+
+        serialize_int( stream, data.a, context.min, context.max );
+        serialize_int( stream, data.b, context.min, context.max );
+
+        serialize_int( stream, data.c, -100, 10000 );
+
+        serialize_varint32( stream, data.v32 );
+        serialize_varint64( stream, data.v64 );
+
+        serialize_bits( stream, data.d, 6 );
+        serialize_bits( stream, data.e, 8 );
+        serialize_bits( stream, data.f, 7 );
+
+        serialize_align( stream );
+
+        serialize_bool( stream, data.g );
+
+        serialize_int( stream, data.numItems, 0, MaxItems - 1 );
+        for ( int i = 0; i < data.numItems; ++i )
+            serialize_bits( stream, data.items[i], 8 );
+
+        // todo
+        // serialize_float( stream, data.float_value );
+
+        // todo: var ints
+
+        // todo
+        // serialize_compressed_float(stream, data.compressed_float_value, 0, 10, 0.01);
+
+        serialize_double( stream, data.double_value );
+
+        serialize_uint64( stream, data.uint64_value );
+
+        serialize_bytes( stream, data.bytes, sizeof( data.bytes ) );
+
+        serialize_string( stream, data.string, sizeof( data.string ) );
+
+        return true;
+    }
+
+    bool operator == ( const TestObject & other ) const
+    {
+        return memcmp( &data, &other.data, sizeof( TestData ) ) == 0;
+    }
+
+    bool operator != ( const TestObject & other ) const
+    {
+        return ! ( *this == other );
+    }
+};
+
+void test_stream()
+{
+    const int BufferSize = 1024;
+
+    uint8_t buffer[BufferSize];
+
+    TestContext context;
+    context.min = -10;
+    context.max = +10;
+
+    WriteStream writeStream( buffer, BufferSize );
+
+    TestObject writeObject;
+    writeObject.Init();
+    writeStream.SetContext( &context );
+    writeObject.Serialize( writeStream );
+    writeStream.Flush();
+
+    const int bytesWritten = writeStream.GetBytesProcessed();
+
+    memset( buffer + bytesWritten, 0, BufferSize - bytesWritten );
+
+    TestObject readObject;
+    ReadStream readStream( buffer, bytesWritten );
+    readStream.SetContext( &context );
+    readObject.Serialize( readStream );
+
+    check( readObject == writeObject );
+}
+
+#define RUN_TEST( test_function )                                           \
+    do                                                                      \
+    {                                                                       \
+        printf( #test_function "\n" );                                      \
+        test_function();                                                    \
+    }                                                                       \
+    while (0)
+
+int main()
+{
+    srand( time( NULL ) );
+
+    printf( "\n" );
+
+    RUN_TEST( test_endian );
+    RUN_TEST( test_bitpacker );
+    RUN_TEST( test_bits_required );
+    RUN_TEST( test_stream );
+
+    printf( "\n*** ALL TESTS PASS ***\n\n" );
+
+    return 0;
+}
