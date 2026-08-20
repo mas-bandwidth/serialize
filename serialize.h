5f0d622975c480f98166a0e5789f138d263837bb
Rowan <rowan@mas-bandwidth.com>
2026-08-21 01:02:47 +1000

    1.11.0: the precomputed compressed-float entry points ship

--- serialize.h
+++ serialize.h
@@ -35,9 +35,9 @@
 /** @file */
 
 #define SERIALIZE_VERSION_MAJOR 1
-#define SERIALIZE_VERSION_MINOR 10
+#define SERIALIZE_VERSION_MINOR 11
 #define SERIALIZE_VERSION_PATCH 0
-#define SERIALIZE_VERSION "1.10.0"
+#define SERIALIZE_VERSION "1.11.0"
 
 #if defined(_MSC_VER)
 #define serialize_restrict __restrict
